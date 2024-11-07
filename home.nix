0cbebda98bc138dbf89fd25c757dba7ab4a860ac
Andre Kurniawan <drekunia@gmail.com>
2024-11-07 13:48:32 +0700

    add google chrome

--- home.nix
+++ home.nix
@@ -15,13 +15,15 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  nixpkgs.config.allowUnfree = true;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
-    # nushell
+    google-chrome
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
